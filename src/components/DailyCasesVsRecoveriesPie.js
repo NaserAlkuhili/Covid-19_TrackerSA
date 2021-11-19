2082c26d39ead4092ed4db3be19788017708bab7
NaserAlkuhili <k4gam3r@gmail.com>
2021-11-19 02:35:35 -0600

    responsive for small screens

--- src/components/DailyCasesVsRecoveriesPie.js
+++ src/components/DailyCasesVsRecoveriesPie.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {Pie} from "react-chartjs-2";
-import "./App.css";
+import "../App.css";
 
 
 import {getCasesFrom, getRecoveriesFrom} from '../API/useAPI';
--- src/components/LineCharts.js
+++ src/components/LineCharts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import "./App.css";
+import "../App.css";
 import {Line} from "react-chartjs-2";
 
 import {getCasesFrom, getRecoveriesFrom} from '../API/useAPI';
